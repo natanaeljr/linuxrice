dba61418f5b50553f37f1328d01b9c5e5e4c12e8
Natanael Rabello <natanael.rabello@outlook.com>
2021-10-06 08:46:44 -0300

    emacs: zeno-theme
    Signed-off-by: Natanael Rabello <natanael.rabello@outlook.com>

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -39,34 +39,36 @@
 ;; THEME
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; https://github.com/bbatsov/solarized-emacs
-(require 'solarized-theme)
-;; make the fringe stand out from the background
-(setq solarized-distinct-fringe-background t)
-;; Don't change the font for some headings and titles
-(setq solarized-use-variable-pitch nil)
-;; make the modeline high contrast
-(setq solarized-high-contrast-mode-line t)
-;; Use less bolding
-(setq solarized-use-less-bold t)
-;; Use more italics
-(setq solarized-use-more-italic t)
-;; Use less colors for indicators such as git:gutter, flycheck and similar
-(setq solarized-emphasize-indicators nil)
-;; Don't change size of org-mode headlines (but keep other size-changes)
-(setq solarized-scale-org-headlines nil)
-;; Avoid all font-size changes
-(setq solarized-height-minus-1 1.0)
-(setq solarized-height-plus-1 1.0)
-(setq solarized-height-plus-2 1.0)
-(setq solarized-height-plus-3 1.0)
-(setq solarized-height-plus-4 1.0)
-;; Load the theme
-(load-theme 'solarized-dark t)
+;; (require 'solarized-theme)
+;; ;; make the fringe stand out from the background
+;; (setq solarized-distinct-fringe-background t)
+;; ;; Don't change the font for some headings and titles
+;; (setq solarized-use-variable-pitch nil)
+;; ;; make the modeline high contrast
+;; (setq solarized-high-contrast-mode-line t)
+;; ;; Use less bolding
+;; (setq solarized-use-less-bold t)
+;; ;; Use more italics
+;; (setq solarized-use-more-italic t)
+;; ;; Use less colors for indicators such as git:gutter, flycheck and similar
+;; (setq solarized-emphasize-indicators nil)
+;; ;; Don't change size of org-mode headlines (but keep other size-changes)
+;; (setq solarized-scale-org-headlines nil)
+;; ;; Avoid all font-size changes
+;; (setq solarized-height-minus-1 1.0)
+;; (setq solarized-height-plus-1 1.0)
+;; (setq solarized-height-plus-2 1.0)
+;; (setq solarized-height-plus-3 1.0)
+;; (setq solarized-height-plus-4 1.0)
+;; ;; Load the theme
+;; (load-theme 'solarized-dark t)
+(require 'zeno-theme)
+(load-theme 'zeno t)
 
-; (add-to-list 'default-frame-alist '(font . "JetBrains Mono" ))
-; (set-face-attribute 'default nil :font "JetBrains Mono-11" )
-(add-to-list 'default-frame-alist '(font . "SF Mono" ))
-(set-face-attribute 'default nil :font "SF Mono-11" )
+(add-to-list 'default-frame-alist '(font . "JetBrains Mono" ))
+(set-face-attribute 'default nil :font "JetBrains Mono-11" )
+; (add-to-list 'default-frame-alist '(font . "SF Mono" ))
+; (set-face-attribute 'default nil :font "SF Mono-11" )
 
 (require 'org-bullets)
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
@@ -85,8 +87,9 @@
  '(custom-safe-themes
    '("b73a23e836b3122637563ad37ae8c7533121c2ac2c8f7c87b381dd7322714cd0" default))
  '(org-agenda-files '("~/notes/FERIAS.org"))
+ '(org-export-backends '(ascii html icalendar latex md odt))
  '(package-selected-packages
-   '(one-themes undo-tree linum-relative use-package solarized-theme org-bullets markdown-mode helm evil-visual-mark-mode)))
+   '(darcula-theme one-themes undo-tree linum-relative use-package solarized-theme org-bullets markdown-mode helm evil-visual-mark-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
