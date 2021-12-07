46323eba11ed0a47c15167cdf85ec53f47f01919
Natanael Rabello <natanael.rabello@outlook.com>
2021-12-07 13:57:21 -0300

    rofi: fix laucher colorful accent color
    Signed-off-by: Natanael Rabello <natanael.rabello@outlook.com>

--- .config/rofi/launchers/colorful/launcher.sh
+++ .config/rofi/launchers/colorful/launcher.sh
@@ -27,9 +27,10 @@ SELECT="#303030ff"
 #SELECT="#f3f3f3ff"
 
 # accent colors
-COLORS=('#EC7875' '#61C766' '#FDD835' '#42A5F5' '#BA68C8' '#4DD0E1' '#00B19F' \
-		'#FBC02D' '#E57C46' '#AC8476' '#6D8895' '#EC407A' '#B9C244' '#6C77BB')
-ACCENT="${COLORS[$(( $RANDOM % 14 ))]}ff"
+#COLORS=('#EC7875' '#61C766' '#FDD835' '#42A5F5' '#BA68C8' '#4DD0E1' '#00B19F' \
+#		'#FBC02D' '#E57C46' '#AC8476' '#6D8895' '#EC407A' '#B9C244' '#6C77BB')
+#ACCENT="${COLORS[$(( $RANDOM % 14 ))]}ff"
+ACCENT="#61C766"
 
 # overwrite colors file
 cat > $dir/colors.rasi <<- EOF
